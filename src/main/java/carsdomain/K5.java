ee2e39e289ad10f991713313383ed2ae859dd43c
정동걸 <ehdrjf2062@gmail.com>
2022-12-08 15:15:49 +0900

    feat(K5): K5 override 작성

--- src/main/java/carsdomain/K5.java
+++ src/main/java/carsdomain/K5.java
@@ -1,4 +1,27 @@
 package carsdomain;
 
-public class K5 {
+public class K5 extends Car{
+    public K5(int distance) {
+        super(distance);
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return 13L;
+    }
+
+    @Override
+    double getTripDistance() {
+        return this.distance;
+    }
+
+    @Override
+    String getName() {
+        return "K5";
+    }
+
+    @Override
+    double getChargeQuantity() {
+        return super.getChargeQuantity();
+    }
 }
