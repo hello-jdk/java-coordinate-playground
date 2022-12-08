33b7e09ddb736f7f0ad9adde61c982274cb6ee81
정동걸 <ehdrjf2062@gmail.com>
2022-12-08 14:56:20 +0900

    test: Injection 수정

--- src/test/java/RentCompanyTest.java
+++ src/test/java/RentCompanyTest.java
@@ -1,3 +1,7 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class RentCompanyTest {
     private static final String NEWLINE = System.getProperty("line.separator");
 
@@ -14,7 +18,7 @@ public class RentCompanyTest {
         assertThat(report).isEqualTo(
                 "Sonata : 15리터" + NEWLINE +
                         "K5 : 20리터" + NEWLINE +
-                        "Sonata : 12리터" + NEWLgINE +
+                        "Sonata : 12리터" + NEWLINE +
                         "Avante : 20리터" + NEWLINE +
                         "K5 : 30리터" + NEWLINE
         );
