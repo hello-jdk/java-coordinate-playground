016de7e63803deeef9267399f4d3811cdbc716d0
정동걸 <ehdrjf2062@gmail.com>
2022-12-08 14:54:15 +0900

    feat: RentCompany

---
+++ src/main/java/RentCompany.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentCompany {
+    private final List<Car> cars;
+
+    private RentCompany(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static RentCompany create() {
+        return new RentCompany(new ArrayList<>());
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public String generateReport() {
+        StringBuilder stringBuilder = new StringBuilder();
+        cars.forEach(car -> {
+            stringBuilder
+                    .append(car.toString())
+                    .append("\n");
+        });
+        return stringBuilder.toString();
+    }
+}
